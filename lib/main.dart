d01be3d54da5b0d9e570959be5630659668214e9
rocky7266 <labnan420@gmail.com>
2025-04-22 22:17:46 +0600

    git start

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'dart:ui_web';
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(app)
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
