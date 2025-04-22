281ef54a4038e0d49b2e6fc74412f04ac7bda677
rocky7266 <labnan420@gmail.com>
2025-04-22 22:32:54 +0600

    basic

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,35 @@ import 'dart:ui_web';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(app)
+  runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter batch 10',
+      home: Home(),
 
+    );
+
+}
+
+  }
+class Home extends StatelessWidget{
+  const Home({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Text("This is body text",
+      style: TextStyle(fontSize: 50,
+      color: Colors.blue),
+      ),
     );
   }
 }
